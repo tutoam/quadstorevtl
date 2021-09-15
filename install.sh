c841c19139356099741d8f6112a447468cce0eca
tutoam <90774759+tutoam@users.noreply.github.com>
2021-09-15 15:19:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 dnf update 
-dnf install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python* 
+dnf install sssd realmd oddjob oddjob-mkhomedir adcli samba-common samba-common-tools krb5-workstation openldap-clients policycoreutils-python* -y
 dnf install httpd
 systemctl enable httpd --now 
  firewall-cmd --add-service http --permanent 
@@ -8,20 +8,20 @@ systemctl enable httpd --now
  firewall-cmd --add-port 3260/tcp --permanent 
  firewall-cmd --reload
  wget https://quadstor.com/vtldownloads/quadstor-vtl-ext-3.0.57-rhel.x86_64.rpm
- dnf install ./quadstor-vtl-ext-3.0.57-rhel.x86_64.rpm 
- dnf upgrade 
- yum upgrade 
- yum install httpd gcc perl kernel-devel sg3_utils iotop sysstat tar
- yum upgrade kernel
- yum upgrade kernel-devel
- dnf upgrade 
+ dnf install ./quadstor-vtl-ext-3.0.57-rhel.x86_64.rpm -y
+ dnf upgrade -y
+ yum upgrade -y
+ yum install httpd gcc perl kernel-devel sg3_utils iotop sysstat tar -y
+ yum upgrade kernel -y
+ yum upgrade kernel-devel -y
+ dnf upgrade -y
  clear
- yum install httpd gcc perl kernel-devel sg3_utils iotop sysstat tar
- yum upgrade kernel
- yum install compat-openssl10 elfutils-libelf-devel policycoreutils policycoreutils-python-utils
- systemctl enable quadstorvtl --now 
- yum -y install httpd gcc perl kernel-devel sg3_utils policycoreutils-python
- setsebool -P httpd_enable_cgi 1
+ yum install httpd gcc perl kernel-devel sg3_utils iotop sysstat tar -y
+ yum upgrade kernel -y
+ yum install compat-openssl10 elfutils-libelf-devel policycoreutils policycoreutils-python-utils -y
+ systemctl enable quadstorvtl --now
+ yum -y install httpd gcc perl kernel-devel sg3_utils policycoreutils-python -y
+ setsebool -P httpd_enable_cgi 1 
  semanage permissive -a httpd_t
  firewall-cmd --permanent --add-port=80/tcp
  firewall-cmd --permanent --add-port=3260/tcp
